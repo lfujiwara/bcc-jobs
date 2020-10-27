abd698626bdd723f86b916dc02240f5a029f4e44
Lucas Toshio Löschner Fujiwara <lucastfujiwara@gmail.com>
2020-10-26 21:59:05 -0300

    Update UserRepository specification Include findAll method

--- src/modules/users/repositories/IUserRepository.ts
+++ src/modules/users/repositories/IUserRepository.ts
@@ -4,4 +4,5 @@ import { IUser } from '../domain/IUser';
 export interface IUserRepository {
   save(user: IUser): Promise<Result<IUser>>;
   findUserByEmail(email: string): Promise<Result<IUser>>;
+  findAll(): Promise<Result<IUser[]>>;
 }
